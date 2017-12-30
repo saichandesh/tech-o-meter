661c0052cc592f6225a44cfadbfba5352f5c5682
saichandesh <saichandesh@gmail.com>
2017-12-30 03:51:17 -0600

    feat(database-set-up) : mysql client with node.js

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,3 +1,16 @@
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD
+});
+  
+con.connect( err => {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
 const login = (req,res) => {
     res.send(`Hello`);
 }
